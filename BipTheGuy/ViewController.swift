5cf3e477bfd683695985b587c3c527128b75b757
Sangha Lee <tkdgklee@gmail.com>
2020-09-20 10:28:31 -0400

    4.5 Working with Alerts

--- BipTheGuy/ViewController.swift
+++ BipTheGuy/ViewController.swift
@@ -30,17 +30,31 @@ class ViewController: UIViewController {
             print("Could not read data from file \(name)")
         }
     }
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func photoOrCameraPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (_) in
+            print("You clicked Photo Library")
+        }
+        let camerAction = UIAlertAction(title: "Camera", style: .default) { (_) in
+            print("You clicked Camera")
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(camerAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
 
-    @IBAction func pucnhButtonPressed(_ sender: UIButton) {
-        let originalImageFrame = imageView.frame
-        let imageWidthShrink: CGFloat = 20
-        let imageHeightShrink: CGFloat = 20
-        let smallerImageFrame = CGRect(x: imageView.frame.origin.x + imageWidthShrink, y: imageView.frame.origin.y + imageHeightShrink, width: imageView.frame.width - (imageWidthShrink * 2), height: imageView.frame.height - (imageHeightShrink * 2))
-        imageView.frame = smallerImageFrame
-        playSound(name: "punchSound")
-        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10.0, animations: {
-            self.imageView.frame = originalImageFrame
-        })
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
